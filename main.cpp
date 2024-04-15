f959d4993a08b59d25ff43a1ec95eed0c04cd3eb
samu <146225785+SamyBravy@users.noreply.github.com>
2024-04-15 16:35:16 +0200

    Added support to load BMP images FIX: added version number

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 #include "BitmapImage.h"
 
+static const std::string VERSION = "v1.0";
+
 int main() {
-    std::cout << "Image viewer" << std::endl;
+    std::cout << "Image viewer - " << VERSION << std::endl;
     BitmapImage b;
     std::string imgName = "test.png";
     if (b.load(imgName))
